ec4d3b96bf1aef4db36d28e55673fa5e5f425a0e
Erez-IL <erez83@gmail.com>
2013-05-20 23:47:50 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,32 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.changePicture();
     },
+    
+    changePicture: function(event) {
+    event.preventDefault();
+    if (!navigator.camera) {
+        app.showAlert("Camera API not supported", "Error");
+        return;
+    }
+    var options =   {   quality: 50,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                        encodingType: 0     // 0=JPG 1=PNG
+                    };
+ 
+    navigator.camera.getPicture(
+        function(imageData) {
+            $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+        },
+        function() {
+            app.showAlert('Error taking picture', 'Error');
+        },
+        options);
+ 
+    return false;
+},
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
